1b3de0445b327d47d0d2658f3bf8a6047e4462d1
alievmuxammed2004-jpg <alievmuxammed2004@gmail.com>
2026-06-28 18:12:57 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ brain_games/conts.py
@@ -0,0 +1,11 @@
+EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+CALC_INSTRUCTION = 'What is the result of the expression?'
+GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
+PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
+PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. ' \
+    'Otherwise answer "no".'
+
+MATH_SIGNS = ('+','-','*','/')
+
+MIN_PROGRESSION_LENGHT, MAX_PROGRESSION_LENGHT = 5, 10
+AMOUNT_OF_ROUNDS = 3
--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,24 +1,24 @@
 import prompt
+from conts import AMOUNT_OF_ROUNDS
 
-ROUNDS_COUNT = 3
 
-def run(game):
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
-    print(f"Hello, {name}!")
+def run_game(get_question_and_answer, instruction):
+   name = prompt.string('Welcome to the Brain Games!\n'
+                        'May i have your name? ')
+   print(f'Hello, {name}!\n'
+         f'{instruction}')
 
-    print(game.DESCRIPTION)
+   for _ in range(AMOUNT_OF_ROUNDS):
+       question, correct_answer = get_question_and_answer()
+       user_answer = prompt.string(f'Question: {question}\n'
+                                    f'Your answer: ')
 
-    for _ in range(ROUNDS_COUNT):
-        question, correct_answer = game.generate_round()
-        print(f"Question: {question}")
-        answer = prompt.string("Your answer: ")
+       if user_answer == correct_answer:
+           print('Correct!')
+       else:
+           print(f"'{user_answer}' is wrong answer ;(. "
+                 f"Correct answer is '{correct_answer}'.\n"
+                 f"Let's try again, {name}!")
+           return
 
-        if answer != correct_answer:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-
-        print("Correct!")
-
-    print(f"Congratulations, {name}!")
+   print(f'Congratulation, {name}!')

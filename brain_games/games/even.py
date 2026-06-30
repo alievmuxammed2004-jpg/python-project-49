5d687df491ec782c22732ff5cec677c60975e774
alievmuxammed2004-jpg <alievmuxammed2004@gmail.com>
2026-06-30 19:44:55 +0300

    new file

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,39 +1,16 @@
 import random
 
+from brain_games.consts import EVEN_INSTRUCTION
 from brain_games.engine import run_game
 
 
-def is_even(number: int) -> bool:
-    """Проверяет, является ли число чётным."""
-    return number % 2 == 0
+def is_play():
+    number = random.randint(1, 100)
+    question = str(number)
+    correct_answer = "yes" if number % 2 == 0 else "no"
+    return question, correct_answer
 
-
-def play_game():
-    print("Welcome to the Brain Games!")
-    name = input("May I have your name? ")
-    print(f"Hello, {name}!")
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
-
-    for _ in range(3):
-        # Генерируем случайное число (например, от 1 до 100)
-        number = random.randint(1, 100)
-        print(f"Question: {number}")
-
-        user_answer = input("Your answer: ").strip().lower()
-
-        correct_answer = "yes" if is_even(number) else "no"
-
-        if user_answer == correct_answer:
-            print("Correct!")
-        else:
-            # В случае неверного ответа выводим сообщение и завершаем игру
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            break
 
 def run_even_game():
-    run_game()
+    run_game(is_play,EVEN_INSTRUCTION)
 
-if __name__ == "__main__":
-    play_game()
